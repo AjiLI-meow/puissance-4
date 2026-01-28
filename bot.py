77d56dbea07ba6c335e2d8ada140f678767f6370
Aijing LI <renoxevans@gmaiil.com>
2026-01-28 20:29:17 +0100

    add random move

---
+++ bot.py
@@ -0,0 +1,10 @@
+import random
+
+# Écrire une fonction coup_aleatoire(g, j) qui joue un coup autorisé au hasard
+# pour le joueur j.
+# (Utiliser randint(a, b) du module random.)
+
+def coup_aleatoire(g,j):
+    return random.randint(0,6)
+
+
--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 from grille import grille_vide, affiche
 from coups import coup_possible, jouer
+from victoire import victoire
 
 def main():
     # g[l][c] line col, joueur 1 ou 2, 0 vide
